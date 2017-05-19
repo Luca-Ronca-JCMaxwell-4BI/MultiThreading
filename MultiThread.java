48839f909624aa01b6a43fda531dabd110e32493
Luca Ronca <luca.ronca@jcmaxwell.it>
2017-05-19 10:17:33 +0200

    Aggiunto monitor

--- MultiThread.java
+++ MultiThread.java
@@ -1,71 +1,76 @@
 package multithread;
 
-import static java.lang.Math.random; //importazione classe Random
+import static java.lang.Math.random;                                            //Importazione classe Random
 import java.util.Random;
-import java.util.concurrent.TimeUnit; 
-import static multithread.TicTacToe.punteggio;  //Importazione della variabile statica punteggio contenuta in TicTacToe
+import java.util.concurrent.TimeUnit;
 
 public class MultiThread {
     
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Main Thread iniziata...");
         long start = System.currentTimeMillis();
-        
-        Thread tic = new Thread(new TicTacToe("TIC"));  // creazione dei thread
-        Thread tac = new Thread(new TicTacToe("TAC"));
-        Thread toe = new Thread(new TicTacToe("TOE"));
-        tic.start();    // partenza dei thread
+        Monitor monitor = new Monitor();
+        Thread tic = new Thread(new TicTacToe("TIC", monitor));                 //Creazione dei thread
+        Thread tac = new Thread(new TicTacToe("TAC", monitor));
+        Thread toe = new Thread(new TicTacToe("TOE", monitor));
+        tic.start();                                                            //Avvio dei thread
         tac.start();
         toe.start();
 
         try {
-            tic.join(); //Aspetta che i thread si interrompano
+            tic.join();                                                         //Aspetta che i thread finiscano la loro esecuzione
             tac.join();
             toe.join();
         } 
         catch (InterruptedException e) {    
         }
-        System.out.println("Punteggio = " + punteggio); //stampa quante volte TOE è venuto dopo TAC
+        System.out.println("Punteggio = " + monitor.getPunteggio());            //Stampa il punteggio
         long end = System.currentTimeMillis();
-        System.out.println("Main Thread completata! tempo di esecuzione: " + (end - start) + "ms");  //stampa il tempo impiegato
+        System.out.println("Main Thread completata! tempo di esecuzione: " + (end - start) + "ms");  
     }
 }
-class TicTacToe implements Runnable {
-    private String t;
+
+class TicTacToe implements Runnable {                                           //Classe dei thread
+    private String nome;
     private String msg;
-    public static boolean conf = false;
-    public static int punteggio = 0;
-    public TicTacToe (String s) {
-        this.t = s;
+    Monitor monitor;
+    //Costruttore
+    public TicTacToe (String n, Monitor m) {
+        nome = n;
+        monitor = m;
     }
-    @Override // Annotazione per il compilatore
-    public void run() {  //istruzioni eseguite dal threrad quando è in funzione
+    @Override                                                                   //Annotazione per il compilatore
+    public void run() {                                                         //Istruzioni eseguite dal threrad quando è in funzione
         for (int i = 10; i > 0; i--) {
-            if("TAC".equals(t))  //controlla se il thread precedente era TAC
-            {
-                conf = true;
-            }
-            Random rand = new Random(); //creazione di un numero casuale
-            int j = 100;
-            int n = 300-j;
-            int tempo = rand.nextInt(n)+j;
-            
-            msg = "<" + t + "> ";
-            try {
-                TimeUnit.MILLISECONDS.sleep(tempo); //attivazione in un tempo casuale
-            } 
-            catch (InterruptedException e) {
-            }
-            if("TOE".equals(t) && conf == true)  //controlla se TOE è venuto dopo TAC
-            {
-                punteggio ++;  // incremento del puntegggio 
-            }
-            else
-            {
-                conf = false; 
-            }
-            msg += t + ": " + i;
+            monitor.incrementaPunteggio(nome, msg);
+            msg = "<" + nome + "> " + nome + ": " + i;
             System.out.println(msg);
         }
     } 
+}
+
+class Monitor {                                                                 //Classe del monitor
+    String last = " ";                                                          //Ultimo thread visualizzato
+    int punteggio = 0;
+    
+    public int getPunteggio() {                                                 //Stampa il punteggio
+        return punteggio;
+    }
+    
+    public synchronized void incrementaPunteggio(String thread, String msg) {   //Metodo condiviso dai thread
+        Random rand = new Random();                                             //Creazione di un numero casuale
+        int j = 100;
+        int n = 300-j;
+        int tempo = rand.nextInt(n)+j;
+        msg += ": " + tempo + " :";
+        if( thread.equals("TOE") && last.equals("TAC")) {                       //Controlla quando TAC viene prima di TIC 
+            punteggio++;                                                        //Se si verifica la condizione aggiorna il punteggio
+        }
+        try {
+            TimeUnit.MILLISECONDS.sleep(tempo);                                 //Thread in pausa per un tempo casuale
+        } 
+        catch (InterruptedException e) {
+        }            
+        last = thread;                                                          //Ultimo thread visualizzato 
+    }
 }
